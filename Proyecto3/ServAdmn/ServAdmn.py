ef113346f35f1b81a747ecc8a10ccdacd359c08e
Raduarez <raduarez@gmail.com>
2020-11-24 14:55:42 -0600

    Update ServAdmn.py

--- Proyecto3/ServAdmn/ServAdmn.py
+++ Proyecto3/ServAdmn/ServAdmn.py
@@ -23,6 +23,7 @@ serv.bind(DIR)
 #Método para atender clientes en multihilo
 def atenderCliente(con, addr):
     conectado = True
+    video = "prueba.mp4"
     print(f"<Nueva Conexion> direccion: {addr} conectada")
     while conectado:
         msg = str(con.recv(BUFFER).decode())
@@ -32,14 +33,15 @@ def atenderCliente(con, addr):
         #Si es servidor, se agregara su objeto de conexion a un arreglo de conexiones servidores
         elif(msg == "servidor"):
             servidores.append(addr[1])
-        if(msg == "video"):
+###########################Se Recibe video crudo del cliente####################
+        elif(msg == "video"):
             print("Recibiendo Video.")
             contenido = con.recv(BUFFER).decode('utf-8')
             archivo, peso = contenido.split("<>")
             archivo = os.path.basename(archivo)
             peso = int(peso)
             barra = tqdm.tqdm(range(peso),f"[->] Recibiendo {archivo}", unit = "B", unit_scale=True, unit_divisor=1024)
-            with open(archivo, "wb") as f:##############################################PARTE QUE FALLA################################################################################
+            with open(archivo, "wb") as f:
                 for _ in barra:
                     lectura = con.recv(BUFFER)
                     if not lectura:
@@ -47,17 +49,41 @@ def atenderCliente(con, addr):
                         barra.close()
                         break;
                     f.write(lectura)
-                    barra.update(len(lectura))#######################################################################################################################################3
+                    barra.update(len(lectura))
                     
                 print("recibido con éxito")
+#############################Fin Recepción de video####################################################
+#############################Se divide el video ####################################################
             print("Diviendo Archivo...")
             servs = 3
             video = archivo
-            print("Dividiendo" + video + "con duración de " + str(get_video_length_s(video)) + "s en " + str(servs) + " servidores")
+            print("Dividiendo " + video + " con duración de " + str(get_video_length_s(video)) + "s en " + str(servs) + " servidores")
             chunk = get_video_length_s(video) / servs
             print("Dividiendo en segmentos de " + str(chunk) + "segundos")
             divide_video(video, chunk)
             print("listo")
+            con.close()
+            conectado = False
+########################Fin de división y cierre de conexión con cliente##################################
+########################Envio de fragmentos a servidores##########################################
+        elif(msg == "recibir"):
+            print("Enviando video fragmentado a servidor")
+            identificador = servidores.index(addr[1])#Identificación para cada servidor
+            archivo = "prueba/"+video[:-4]+str(identificador)+".mp4" 
+            mandarVideo(archivo,con)
+            conectado = False
+########################Fin de envio fragmentos a servidores##########################################
+        if(msg == "recibir-fragmentos"):
+            print("hola")
+        if(msg == "cliente-final"):
+            print("hola")
+                
+                 
+                
+#            print("Enviando video fragmentado a servidor")
+#            identificador = servidores.index(addr[1])#Identificación para cada servidor
+#            archivo = "prueba/"+video[:-4]+str(identificador)+".mp4"
+#            print(archivo);
         #conocerConexion(msg, con, addr)
         print(f"<Clientes> {clientes}")
         print(f"<Servidores> {servidores}")
@@ -84,6 +110,25 @@ def start():
         #Mostrar conexion activas, por alguna razón a mi me marca 4 iniciales
         #Nota, cambiar la resta si se muestra diferente a las conexiones reales.
         print(f"<Conexiones Activas> {threading.activeCount()-5}")
+#########Mandar Video###############################
+def mandarVideo(archivo,cliente):
+    peso = os.path.getsize(str(archivo))
+    previo = f"{archivo}<>{peso}"
+    input()
+    mandarMsg(previo,cliente)
+    barra = tqdm.tqdm(range(peso),f"[->] Enviado {archivo}", unit = "B", unit_scale=True, unit_divisor=1024)
+    with open(archivo,"rb") as f:
+        for _ in barra:
+            lectura = f.read(BUFFER)
+            if not lectura:
+                barra.close()
+                break
+            cliente.sendall(lectura)
+            barra.refresh()
+            barra.update(len(lectura))
+    print(f"{archivo} enviado con éxito!!")
+    cliente.close()        
+
 #Métodos de división de video.
 def get_video_length_s(filename):
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
